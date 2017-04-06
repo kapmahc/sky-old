6d22eb8411007fc0e1f09b932822f7c9c392794f
Jeremy Zheng <jitang.zheng@gmail.com>
2017-04-06 00:50:15 -0700

    datasource

--- config.go
+++ config.go
@@ -1,8 +1,34 @@
 package sky
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 // IsProduction production mode ?
 func IsProduction() bool {
 	return viper.GetString("env") == "production"
+}
+
+// DataSource database source url
+func DataSource() string {
+	//"user=%s password=%s host=%s port=%d dbname=%s sslmode=%s"
+	args := ""
+	for k, v := range viper.GetStringMapString("database.args") {
+		args += fmt.Sprintf(" %s=%s ", k, v)
+	}
+	return args
+
+	//"postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full")
+	// return fmt.Sprintf(
+	// 	"%s://%s:%s@%s:%d/%s?sslmode=%s",
+	// 	viper.GetString("database.driver"),
+	// 	viper.GetString("database.args.user"),
+	// 	viper.GetString("database.args.password"),
+	// 	viper.GetString("database.args.host"),
+	// 	viper.GetInt("database.args.port"),
+	// 	viper.GetString("database.args.dbname"),
+	// 	viper.GetString("database.args.sslmode"),
+	// )
 }
